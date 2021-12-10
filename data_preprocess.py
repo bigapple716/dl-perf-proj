6c6764583c6d8b417426b4bc2d5b95817daf1cba
bigapple716 <congcong0716@outlook.com>
2021-12-10 11:15:05 -0500

    data_preprocess.py finished

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,18 +1,65 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer
+import tensorflow as tf
+
+# hyper parameters
+BATCH_SIZE = 8
 
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
-wiki_small = load_dataset("wikitext", "wikitext-2-v1")
-wiki_large = load_dataset("wikitext", "wikitext-103-v1")
 
-raw_datasets = wiki_small
+def tokenize_function(example):
+    return tokenizer(example['text'], max_length=512, padding='max_length', truncation=True)
 
-# print dataset info
-print('Dataset Info - wikitext-2')
-print(wiki_small)
-print('Features:', wiki_small['train'].features)
-print('Example:', wiki_small['train'][4])
 
-batches = tokenizer(raw_datasets, max_length=512, padding='max_length', truncation=True)
-print(batches)
+def preprocess(use_small_dataset=True):
+    wiki_small = load_dataset("wikitext", "wikitext-2-v1")
+    wiki_large = load_dataset("wikitext", "wikitext-103-v1")
+
+    if use_small_dataset:
+        raw_datasets = wiki_small
+    else:
+        raw_datasets = wiki_large
+
+    # print dataset info
+    print('Dataset Info:')
+    print(wiki_small)
+    print('Features:', wiki_small['train'].features)
+    print('Example:', wiki_small['train'][4])
+    print()
+
+    # map tokenizer to datasets
+    tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+
+    # print dataset info
+    print('Tokenized Dataset Info:')
+    print(tokenized_datasets)
+
+    # Arrow datasets -> tf datasets
+    tf_train_dataset = tokenized_datasets["train"].to_tf_dataset(
+        columns=["attention_mask", "input_ids", "token_type_ids"],
+        shuffle=True,
+        batch_size=BATCH_SIZE,
+    )
+    tf_validation_dataset = tokenized_datasets["validation"].to_tf_dataset(
+        columns=["attention_mask", "input_ids", "token_type_ids"],
+        shuffle=False,
+        batch_size=BATCH_SIZE,
+    )
+    tf_test_dataset = tokenized_datasets["test"].to_tf_dataset(
+        columns=["attention_mask", "input_ids", "token_type_ids"],
+        shuffle=False,
+        batch_size=BATCH_SIZE,
+    )
+
+    # print dataset info
+    print('tf datasets:')
+    print(tf_train_dataset)
+    print(tf_validation_dataset)
+    print(tf_test_dataset)
+
+    return tf_train_dataset, tf_validation_dataset, tf_test_dataset
+
+
+if __name__ == '__main__':
+    preprocess()
