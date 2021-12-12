68876d97caff5a9f774af15b6e28b1fde5226d83
bigapple716 <congcong0716@outlook.com>
2021-12-12 12:26:01 -0500

    delete useless code from data_preprocess.py

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,6 +1,5 @@
 from datasets import load_dataset
 from transformers import AutoTokenizer
-import tensorflow as tf
 
 # hyper parameters
 BATCH_SIZE = 8
@@ -23,46 +22,14 @@ def preprocess(use_small_dataset=True):
 
     # print dataset info
     print('Dataset Info:')
-    print(wiki_small)
-    print('Features:', wiki_small['train'].features)
-    print('Example:', wiki_small['train'][4])
+    print(raw_datasets)
+    print('Features:', raw_datasets['train'].features)
+    print('Example:', raw_datasets['train'][4])
     print()
 
     with open('dump.txt', 'w') as f_out:
-        for row in wiki_small['train']:
+        for row in raw_datasets['train']:
             f_out.write(row['text'])
-
-    # # map tokenizer to datasets
-    # tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
-    #
-    # # print dataset info
-    # print('Tokenized Dataset Info:')
-    # print(tokenized_datasets)
-    #
-    # # Arrow datasets -> tf datasets
-    # tf_train_dataset = tokenized_datasets["train"].to_tf_dataset(
-    #     columns=["attention_mask", "input_ids", "token_type_ids"],
-    #     shuffle=True,
-    #     batch_size=BATCH_SIZE,
-    # )
-    # tf_validation_dataset = tokenized_datasets["validation"].to_tf_dataset(
-    #     columns=["attention_mask", "input_ids", "token_type_ids"],
-    #     shuffle=False,
-    #     batch_size=BATCH_SIZE,
-    # )
-    # tf_test_dataset = tokenized_datasets["test"].to_tf_dataset(
-    #     columns=["attention_mask", "input_ids", "token_type_ids"],
-    #     shuffle=False,
-    #     batch_size=BATCH_SIZE,
-    # )
-    #
-    # # print dataset info
-    # print('tf datasets:')
-    # print(tf_train_dataset)
-    # print(tf_validation_dataset)
-    # print(tf_test_dataset)
-    #
-    # return tf_train_dataset, tf_validation_dataset, tf_test_dataset
 
 
 if __name__ == '__main__':
