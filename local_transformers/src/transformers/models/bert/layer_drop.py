ff6830f39cbb61def9822c1b2c236adbbf5efd03
bigapple716 <congcong0716@outlook.com>
2021-12-14 15:21:07 -0500

    delete useless codes

--- local_transformers/src/transformers/models/bert/layer_drop.py
+++ local_transformers/src/transformers/models/bert/layer_drop.py
@@ -1,24 +1,8 @@
-# Copyright (c) Facebook, Inc. and its affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-"""
-LayerDrop as described in https://arxiv.org/abs/1909.11556.
-"""
-
 import torch
 import torch.nn as nn
 
 
 class LayerDropModuleList(nn.ModuleList):
-    """
-    A LayerDrop implementation based on :class:`torch.nn.ModuleList`.
-
-    We refresh the choice of which layers to drop every time we iterate
-    over the LayerDropModuleList instance. During evaluation we always
-    iterate over all layers.
-    """
-
     def __init__(self, p, modules=None):
         super().__init__(modules)
         self.p = p
